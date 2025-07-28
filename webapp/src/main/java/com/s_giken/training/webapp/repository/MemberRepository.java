bc311084b319063a9b5856b16436645f7c89338d
Ohshima Yusuke (s-giken) <ooshima@s-giken.com>
2025-07-29 07:38:11 +0900

    あいまいなコメントの見直し

--- webapp/src/main/java/com/s_giken/training/webapp/repository/MemberRepository.java
+++ webapp/src/main/java/com/s_giken/training/webapp/repository/MemberRepository.java
@@ -9,7 +9,7 @@ public interface MemberRepository extends JpaRepository<Member, Integer> {
     /*
      * SprintBootでは、JpaRepositioyを継承したインターフェースを元に実態クラスが自動生成されます。
      * 
-     * 【予め用意されているメソッド】
+     * 【自動生成されるメソッド】
      * <S extends T> S save(S entity); // エンティティの保存
      * T findOne(ID primaryKey); // 主キーを元にエンティティを取得
      * Iterable<T> findAll(); // 全てのエンティティを取得
@@ -19,7 +19,8 @@ public interface MemberRepository extends JpaRepository<Member, Integer> {
      * 
      * 別途メソッドを定義する場合はルールに従ってメソッド名を定義する必要があります。
      * 詳細は以下の参考先URLを確認して下さい。
-     * https://docs.spring.io/spring-data/jpa/docs/1.11.1.RELEASE/reference/html/# jpa.query-methods
+     * 
+     * https://docs.spring.io/spring-data/jpa/docs/1.11.1.RELEASE/reference/html/#jpa.query-methods
      */
 
     // TODO: 名前でも検索できるようにメソッドを定義しなおす
